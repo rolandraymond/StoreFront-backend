dd350f6809c36675dd8646e658a5fc9864dd2d22
rowlandraymond <rolandremon300@gmail.com>
2022-09-24 00:28:43 +0200

    change convertDB to NODE_ENV

--- build/config.js
+++ build/config.js
@@ -7,13 +7,13 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const { POSTGRES_HOST, NODE_ENV, POSTGRES_DB_TEST, //   test  DataBase
 POSTGRES_DB, //    dev DataBase
-POSTGRES_USER, POSTGRES_PASSWORD, BCRYPT_pass, round_hash, token_secret, } = process.env;
+POSTGRES_USER, POSTGRES_PASSWORD, BCRYPT_PASS, ROUND_HASH, TOKEN_SECRET, } = process.env;
 exports.default = {
     host: POSTGRES_HOST,
     database: NODE_ENV === 'test' ? POSTGRES_DB_TEST : POSTGRES_DB,
     user: POSTGRES_USER,
     password: POSTGRES_PASSWORD,
-    crypt_pass: BCRYPT_pass,
-    hashround: round_hash,
-    token_secret: token_secret,
+    crypt_pass: BCRYPT_PASS,
+    hashround: ROUND_HASH,
+    token_secret: TOKEN_SECRET,
 };
